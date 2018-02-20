6a401a7923f482061f4e9ec8c61d5ee7181f94ba
BraddockG <braddockgerome@gmail.com>
2018-02-20 20:18:40 +0000

    first commit

---
+++ emlintests/EmlinKeyboardTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace EmlinTests
+{
+    [TestFixture]
+    public class EmlinKeyboardTest
+    {
+        [Test]
+        public void CanDoWhatever()
+        {
+            Assert.That(1 == 2);
+        }
+    }
+}
