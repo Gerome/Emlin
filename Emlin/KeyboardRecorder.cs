54d3627c9fb44c7c4047301539d65ae1a2ae08b0
BraddockG <braddockgerome@gmail.com>
2018-02-28 21:25:14 +0000

    Written tests to see if character initializers are set up correctly

--- Emlin/KeyboardRecorder.cs
+++ Emlin/KeyboardRecorder.cs
@@ -10,22 +10,80 @@ namespace Emlin
 {
     public class KeyboardRecorder
     {
-        private char[] listOfInputs;
-        private int numberOfInputs = Enum.GetNames(typeof(Keys)).Length;
+        private int DECIMAL_ASCII_OF_FIRST_CHAR = 32;
 
+        private int numberOfInputs;
+        private int numberOfCombinations;
+
+        private char[] listOfInputs;
+        private Dictionary<int, List<char>> dictOfCombinations;
         private List<char> keysEntered = new List<char>();
 
+        public char[] ListOfInputs { get => listOfInputs; set => listOfInputs = value; }
+        public Dictionary<int, List<char>> DictOfCombinations { get => dictOfCombinations; set => dictOfCombinations = value; }
+
+
+        //public List<char> KeysEntered { get => keysEntered; set => keysEntered = value; }
+
+        //List<long> ticks = new List<long>();
+        //List<long> timeDif = new List<long>();
+        //List<long> timeDifInMillis = new List<long>();
+        //Tuple<char, long> dict
         public KeyboardRecorder()
         {
+            numberOfInputs = 128 - 32;
+            numberOfCombinations = numberOfInputs * numberOfInputs;
+            // Number of combinations is the number of inputs squared
+            //numberOfCombinations = numberOfInputs * numberOfInputs;
+
             ListOfInputs = new char[numberOfInputs];
+            DictOfCombinations = new Dictionary<int, List<char>>();
+            PopulateListOfInputs();
+            PopulateListOfCombinations();
+
         }
 
-        public char[] ListOfInputs { get => listOfInputs; set => listOfInputs = value; }
-        public List<char> KeysEntered { get => keysEntered; set => keysEntered = value; }
+        private void PopulateListOfCombinations()
+        {
+            int index;
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                for (int n = 0; n < numberOfInputs; n++)
+                {
+                    index = i * numberOfInputs + n;
+                    DictOfCombinations.Add(index, new List<char>() { ListOfInputs[i], ListOfInputs[n] });
+                }
+            }
+        }
+
+        private void PopulateListOfInputs()
+        {
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                listOfInputs[i] = (char)(i + DECIMAL_ASCII_OF_FIRST_CHAR);
+            }
+        }
+
+        #region public methods
 
         public void Keypressed(Object o, KeyPressEventArgs e)
         {
-            KeysEntered.Add(e.KeyChar);         
+            //ticks.Add(DateTime.Now.Ticks);
+
+            //if (timeDif.Count == 0)
+            //{
+            //    timeDif.Add(ticks[0]);
+            //}
+            //else
+            //{
+            //    timeDif.Add(ticks[ticks.Count - 1] - ticks[ticks.Count - 2]);
+            //}
+            //timeDifInMillis.Add(timeDif[timeDif.Count - 1] / 10000);
+            //dict.Add(e.KeyChar, timeDifInMillis[timeDifInMillis.Count - 1]);
+            //KeysEntered.Add(e.KeyChar);             
         }
+
+
+        #endregion
     }
 }
--- EmlinTests/EmlinKeyboardTests.cs
+++ EmlinTests/EmlinKeyboardTests.cs
@@ -11,41 +11,105 @@ namespace EmlinTests
     public class EmlinKeyboardTests
     {
         char[] listOfInputs;
-        List<char> listOfKeysEntered;
+        Dictionary<int, List<char>> dictOfCombinations;
+        //List<char> listOfKeysEntered;
         KeyboardRecorder kbRec = new KeyboardRecorder();
+
+
+        //private int DECIMAL_ASCII_OF_FIRST_CHAR = 32;
+        //private int DECIMAL_ASCII_OF_LAST_CHAR  = 128;
+        private int NUMBER_OF_INPUTS            = 96;
+        private int NUMBER_OF_COOMBINATIONS     = 96 * 96;
 
         public EmlinKeyboardTests()
         {
             
             listOfInputs = kbRec.ListOfInputs;
-            listOfKeysEntered = kbRec.KeysEntered;
+            dictOfCombinations = kbRec.DictOfCombinations;
+            //listOfKeysEntered = kbRec.KeysEntered;
+            
         }
 
         [Test]
-        public void KEYBOARD_RECORDER_SHOULD_CREATE_AN_ARRAY()
+        public void KEYBOARD_RECORDER_SHOULD_CREATE_AN_ARRAY_OF_LIST_OF_INPUTS()
         {
-            Assert.NotNull(listOfInputs, null);
+            Assert.NotNull(listOfInputs);
         }
 
         [Test]
-        public void KEYBOARD_RECORDER_INPUT_LIST_SHOULD_BE_202_IN_LENGTH()
+        public void LIST_OF_INPUTS_SHOULD_BE_96_CHARACTERS_IN_LENGTH()
         {
-            int numberOfKeyInputs = 194; 
-            Assert.That(numberOfKeyInputs, Is.EqualTo(listOfInputs.Length));
-        }
-        
-        [Test]
-        public void PRESSING_A_KEY_SHOULD_ADD_KEY_TO_ARRAYLIST_OF_PRESSED_KEYS()
-        {
-            kbRec.Keypressed(null, new KeyPressEventArgs('A'));         
-            Assert.That(listOfKeysEntered.Contains('A'));
+            Assert.That(listOfInputs.Length, Is.EqualTo(NUMBER_OF_INPUTS));
         }
 
         [Test]
-        public void PRESSING_A_KEY_SHOULD_ADD_ANOTHER_KEY_TO_ARRAYLIST_OF_PRESSED_KEYS()
+        public void FIRST_CHARACTER_IN_LIST_OF_INPUTS_SHOULD_BE_THE_SPACE_CHARACTER()
         {
-            kbRec.Keypressed(null, new KeyPressEventArgs('B'));
-            Assert.That(listOfKeysEntered.Contains('B'));
+            Assert.That(listOfInputs[0], Is.EqualTo((char)Keys.Space));
         }
+
+        [Test]
+        public void LAST_CHARACTER_IN_LIST_OF_INPUTS_SHOULD_BE_THE_DELETE_CHARACTER()
+        {
+            Assert.That(listOfInputs[NUMBER_OF_INPUTS - 1], Is.EqualTo('\u007f'));
+        }
+
+        [Test]
+        public void KEYBOARD_RECORDER_SHOULD_CREATE_AN_ARRAY_OF_LIST_OF_COMBINATIONS()
+        {
+            Assert.NotNull(dictOfCombinations);
+        }
+
+        [Test]
+        public void LIST_OF_COMBINATIONS_SHOULD_BE_9216_IN_LENGTH()
+        {
+            Assert.That(dictOfCombinations.Count, Is.EqualTo(NUMBER_OF_COOMBINATIONS));
+        }
+
+        [Test]
+        public void FIRST_VALUE_IN_COMBINATIONS_SHOULD_SPACE_SPACE()
+        {
+            
+            Assert.That(dictOfCombinations[0], Is.EqualTo(new List<char>() { ' ', ' ' }));
+        }
+
+        [Test]
+        public void LAST_VALUE_IN_COMBINATIONS_SHOULD_SPACE_SPACE()
+        {
+
+            Assert.That(dictOfCombinations[NUMBER_OF_COOMBINATIONS - 1], Is.EqualTo(new List<char>() { '\u007f', '\u007f' }));
+        }
+
+        //[Test]
+        //public void PRESSING_A_KEY_SHOULD_ADD_KEY_TO_ARRAYLIST_OF_PRESSED_KEYS()
+        //{
+        //    //kbRec.Keypressed(null, new KeyPressEventArgs('A'));         
+        //    //Assert.That(listOfKeysEntered.Contains('A'));
+        //}
+
+        //[Test]
+        //public void PRESSING_A_KEY_SHOULD_ADD_ANOTHER_KEY_TO_ARRAYLIST_OF_PRESSED_KEYS()
+        //{
+        //    //kbRec.Keypressed(null, new KeyPressEventArgs('B'));
+        //    //Assert.That(listOfKeysEntered.Contains('B'));
+        //}
+
+        //[Test]
+        //public void PRESSING_TWO_KEYS_AT_DIFFERENT_TIMES_SHOULD_RECORD_THE_DIFFERENCE()
+        //{
+        //    //kbRec.Keypressed(null, new KeyPressEventArgs('A'));
+        //    //Thread.Sleep(new TimeSpan(300000));
+        //    //kbRec.Keypressed(null, new KeyPressEventArgs('B'));
+        //    //Assert.That(keysTyped)
+        //}
+
+        /*
+            * --TEST LIST--
+            * List of combinations should contain all possible combinations
+            * 
+            * 
+            * PRESSING TWO KEYS IN SUCCESSION SHOULD RECORD BOTH KEYS AND THE TICKS BETWEEN THEM
+            * TWO KEYS PRESSED IN LESS THAN 1.5(?) SECONDS SHOULD NOT RECORD THE DIFFERENCE
+            */
     }
 }
