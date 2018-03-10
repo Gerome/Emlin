3bbdcc90a38bfd109f832533daaf641acb262c9d
BraddockG <braddockgerome@gmail.com>
2018-03-10 14:38:36 +0000

    Created class to write data to file and set up mock file system for testing purposes

--- Emlin/TimeToFileRecorder.cs
+++ Emlin/TimeToFileRecorder.cs
@@ -1,7 +1,23 @@
-﻿
+﻿using System;
+using System.IO.Abstractions;
+
 namespace Emlin
 {
-    class TimeToFileRecorder
+    public class TimeToFileRecorder
     {
+        readonly IFileSystem fileSystem;
+
+        public TimeToFileRecorder(IFileSystem fileSystem)
+        {
+            this.fileSystem = fileSystem;
+        }
+
+        public TimeToFileRecorder():this(fileSystem: new FileSystem())
+        {}
+
+        public void WriteRecordedDataToFile(KeyCombination[] keyCombinations)
+        {
+            //throw new NotImplementedException();
+        }
     }
 }
--- EmlinTests/TimeToFileRecorderTests.cs
+++ EmlinTests/TimeToFileRecorderTests.cs
@@ -1,9 +1,33 @@
 ﻿using Emlin;
 using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
 
 namespace EmlinTests
 {
     class TimeToFileRecorderTests
     {
+        private KeyCombination[] keyCombinations;
+        private MockFileSystem fileSystem;
+        private TimeToFileRecorder ttfRecorder;
+
+        [SetUp]
+        public void Init()
+        {
+            fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { @"C:\File\KeyboardData.txt", new MockFileData("") }
+            });
+            
+            ttfRecorder = new TimeToFileRecorder(fileSystem);
+        }
+
+        [Test]
+        public void CALLING_WRITE_TO_FILE_SHOULD_ADD_CURRENT_DATA_TO_FILE()
+        {
+            keyCombinations = new KeyCombination[0];
+            ttfRecorder.WriteRecordedDataToFile(keyCombinations);
+
+        }
     }
 }
