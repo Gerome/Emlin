531742a2f7b53f87d436a1edb4c59ffd6f1cbfec
BraddockG <braddockgerome@gmail.com>
2019-01-17 12:05:31 +0000

    Clearning the python file

--- Emlin/Python/EmlinSVM.py
+++ Emlin/Python/EmlinSVM.py
@@ -1,11 +1,14 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 import os
 import pandas as pd
+
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+
 DATA_PATH = "C:/Users/Gerome/Dropbox/CI301-The Individual Project/Data/Processed"
 
 def load_group_data(data_path=DATA_PATH):
@@ -13,39 +16,9 @@ def load_group_data(data_path=DATA_PATH):
     return pd.read_csv(csv_path)
 
 
-# In[2]:
-
-
 group_data = load_group_data()
 group_data.head()
 
-
-# In[4]:
-
-
-from sklearn.model_selection import train_test_split
-train_set, test_set = train_test_split(group_data, test_size = 0.2, random_state=2)
-
-
-# In[5]:
-
-
-print(len(train_set), "train +", len(test_set), "test")
-
-
-# In[6]:
-
-
-train_set.info()
-
-
-# In[9]:
-
-import numpy as np
-from sklearn.svm import SVC
-from sklearn.model_selection import cross_val_score
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
 
 all_x = group_data[['Id', 'HT', 'FT', 'Di1', 'Di2', 'Di3']].values
 all_y = group_data[['User']].values
@@ -59,7 +32,4 @@ svm_clf = Pipeline((
 svm_scores = cross_val_score(svm_clf, all_x, all_y, cv=5, verbose=True, n_jobs=5)
 
 print(svm_scores.mean())
-# In[8]:
-from sklearn.externals import joblib
-svm_clf.fit(all_x, all_y)
 
