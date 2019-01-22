11f071791df25e8ee6cd13557beb979cf3c18324
BraddockG <braddockgerome@gmail.com>
2019-01-22 19:39:50 +0000

    Organising file structure

--- Emlin/TestMembers/CustomTimer.cs
+++ Emlin/TestMembers/CustomTimer.cs
@@ -1,13 +1,12 @@
-﻿
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace Emlin
 {
     class CustomTimer : Timer, ITimerInterface
-    {     
+    {
         public CustomTimer(int interval)
         {
             this.Interval = interval;
-        }     
+        }
     }
 }
